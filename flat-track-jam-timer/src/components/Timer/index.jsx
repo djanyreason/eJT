4a1731efa9cf5418004b702afb3c7779571a6c02
John Anderson <djanyreason@gmail.com>
2024-06-30 11:28:41 -0500

    Cleans up Timer and useTimer files
    This commit removes commented out code.
    
    It also removes the 'limit' member of the Timer state, as that was
    redundant with the introduction of the limitTime member
    (limitTime > 0 is equivalent to limit)

--- flat-track-jam-timer/src/components/Timer/index.jsx
+++ flat-track-jam-timer/src/components/Timer/index.jsx
@@ -12,7 +12,7 @@ const periodTime = [30, 0];
 const jamTime = [2, 0];
 const lineupTime = [0, 30];
 
-const msTime = (time) => 100 * (time[0] * 60 + time[1]);
+const msTime = (time) => 1000 * (time[0] * 60 + time[1]);
 
 const jamTimeMS = msTime(jamTime);
 const periodTimeMS = msTime(periodTime);
@@ -152,17 +152,18 @@ const Timer = () => {
   ]);
 
   useEffect(() => {
-    if (periodTimer.limit) {
+    if (periodTimer.limitTime > 0) {
       setClickState(GameStateEnum.INTERMISSION);
       setLimitState(GameStateEnum.INTERMISSION);
       if (currentState === GameStateEnum.LINEUP)
         updateState(GameStateEnum.INTERMISSION, periodTimer.limitTime);
     }
-  }, [currentState, periodTimer.limit, periodTimer.limitTime, updateState]);
+  }, [currentState, periodTimer.limitTime, updateState]);
 
   useEffect(() => {
-    if (secondTimer.limit) updateState(limitState, secondTimer.limitTime);
-  }, [limitState, secondTimer.limit, secondTimer.limitTime, updateState]);
+    if (secondTimer.limitTime > 0)
+      updateState(limitState, secondTimer.limitTime);
+  }, [limitState, secondTimer.limitTime, updateState]);
 
   const updateState = useCallback(
     (newState, currTime) => {
@@ -221,149 +222,6 @@ const Timer = () => {
     },
     [updateState]
   );
-  /*
-  const testTimer = useTimer(30 * 1000);
-  const resetAndStart = async () => {
-    await testTimer.dispatch({ type: timerDispatch.RESET });
-    await testTimer.dispatch({ type: timerDispatch.START });
-  };
-
-  const testObj = useMemo(() => {
-    return {
-      name: 'test',
-      func: () => console.log('callback'),
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log(testObj.name);
-  }, [testObj]);
-
-  return (
-    <View style={styles.container}>
-      <View style={dimensionBoxStyle}>
-        <Display
-          style={styles.textStyle}
-          limit={testTimer.maxTime}
-          time={testTimer.time}
-          countdown={true}
-        />
-      </View>
-      <View style={dimensionBoxStyle}>
-        <ControlButton
-          style={styles.textStyle}
-          label={!testTimer.timer.paused ? 'Pause' : 'Start'}
-          onPress={
-            !testTimer.timer.paused
-              ? () => testTimer.dispatch({ type: timerDispatch.PAUSE })
-              : () => testTimer.dispatch({ type: timerDispatch.START })
-          }
-        />
-      </View>
-      <View style={dimensionBoxStyle}>
-        <ControlButton
-          style={styles.textStyle}
-          label={'Reset and Stop'}
-          onPress={() => testTimer.dispatch({ type: timerDispatch.RESET })}
-        />
-      </View>
-      <View style={dimensionBoxStyle}>
-        <ControlButton
-          style={styles.textStyle}
-          label={'Reset and Run'}
-          onPress={resetAndStart}
-        />
-      </View>
-    </View>
-  );
-
-  const [periodTimer, periodDispatch] = useTimer(periodTimeMS);
-  const [secondTimer, secondDispatch] = useTimer(jamTimeMS);
-
-  const [currentState, setCurrentState] = useState(GameStateEnum.COMING_UP);
-  const [clickState, setClickState] = useState(GameStateEnum.JAM);
-  const [limitState, setLimitState] = useState(GameStateEnum.COMING_UP);
-
-  const [clockCountdown, setClockCountdown] = useState(true);
-  const [buttonLabel, setButtonLabel] = useState('Start Jam');
-
-  const resetAll = useCallback(() => {
-    periodDispatch({ type: timerDispatch.RESET });
-    secondDispatch({
-      type: timerDispatch.RESET,
-      payload: { maxTime: jamTimeMS },
-    });
-    setButtonLabel('Start Jam');
-    setClickState(GameStateEnum.JAM);
-    setLimitState(GameStateEnum.COMING_UP);
-    setClockCountdown(true);
-  }, [periodDispatch, secondDispatch]);
-
-  useEffect(() => {
-    if (periodTimer.timer.limit) {
-      setClickState(GameStateEnum.INTERMISSION);
-      setLimitState(GameStateEnum.INTERMISSION);
-      if (currentState === GameStateEnum.LINEUP)
-        updateState(GameStateEnum.INTERMISSION);
-    }
-  }, [currentState, periodTimer.timer.limit, updateState]);
-
-  useEffect(() => {
-    if (secondTimer.timer.limit) updateState(limitState);
-  }, [limitState, secondTimer.timer.limit, updateState]);
-
-  const updateState = useCallback(
-    (newState) => {
-      setCurrentState(newState);
-      switch (newState) {
-        case GameStateEnum.JAM:
-          periodDispatch({ type: timerDispatch.START });
-          secondDispatch({
-            type: timerDispatch.RESET,
-            payload: { maxTime: jamTimeMS },
-          });
-          secondDispatch({ type: timerDispatch.START });
-          setButtonLabel('End Jam');
-          setClockCountdown(true);
-          setClickState(GameStateEnum.LINEUP);
-          setLimitState(GameStateEnum.LINEUP);
-          break;
-        case GameStateEnum.LINEUP:
-          secondDispatch({
-            type: timerDispatch.RESET,
-            payload: { maxTime: lineupTimeMS },
-          });
-          secondDispatch({ type: timerDispatch.START });
-          setButtonLabel('Call Timeout');
-          setClockCountdown(false);
-          setClickState(GameStateEnum.TIMEOUT);
-          setLimitState(GameStateEnum.JAM);
-          break;
-        case GameStateEnum.TIMEOUT:
-          periodDispatch({ type: timerDispatch.PAUSE });
-          secondDispatch({
-            type: timerDispatch.RESET,
-            payload: { maxTime: 0 },
-          });
-          secondDispatch({ type: timerDispatch.START });
-          setButtonLabel('Start Jam');
-          setClockCountdown(false);
-          setClickState(GameStateEnum.JAM);
-          setLimitState(GameStateEnum.TIMEOUT);
-          break;
-        case GameStateEnum.INTERMISSION:
-          secondDispatch({
-            type: timerDispatch.RESET,
-            payload: { maxTime: jamTimeMS },
-          });
-          setClockCountdown(true);
-          setButtonLabel('Intermission');
-          setClickState(GameStateEnum.COMING_UP);
-          setLimitState(GameStateEnum.COMING_UP);
-      }
-    },
-    [periodDispatch, secondDispatch]
-  );*/
 
   return (
     <View style={styles.container}>
@@ -399,119 +257,6 @@ const Timer = () => {
       </View>
     </View>
   );
-
-  /*
-  const periodTimeMS = (periodTime[0] * 60 + periodTime[1]) * 1000;
-  const jamTimeMS = (jamTime[0] * 60 + jamTime[1]) * 1000;
-  const lineupTimeMS = (lineupTime[0] * 60 + lineupTime[1]) * 1000;
-  const lineupFunc = () => {
-    console.log('Hello');
-  };
-  const periodTimer = useTimer(periodTimeMS);
-  const jamTimer = useTimer(jamTimeMS);
-  const lineupTimer = useTimer(lineupTimeMS, lineupFunc);
-  const timeoutTimer = useTimer();
-
-  const [buttonLabel, setButtonLabel] = useState('Start Jam');
-
-  const endTimeout = () => {
-    timeoutTimer.pauseTimer();
-    timeoutTimer.resetTimer();
-    periodTimer.startTimer();
-    jamTimer.startTimer();
-    setButtonLabel('End Jam');
-  };
-
-  const endJam = useCallback(() => {
-    const resetJam = jamTimer.resetTimer;
-    resetJam();
-    if (periodTimer.limit) {
-      setButtonLabel('Reset Clocks');
-    } else {
-      const startLineup = lineupTimer.startTimer;
-      startLineup();
-      setButtonLabel('Timeout');
-    }
-  }, [jamTimer.resetTimer, periodTimer.limit, lineupTimer.startTimer]);
-
-  const callTimeout = () => {
-    lineupTimer.resetTimer();
-    periodTimer.pauseTimer();
-    timeoutTimer.startTimer();
-    setButtonLabel('Start Jam');
-  };
-
-  const resetAll = () => {
-    periodTimer.resetTimer();
-    jamTimer.resetTimer();
-    lineupTimer.resetTimer();
-    timeoutTimer.resetTimer();
-    setButtonLabel('Start Jam');
-  };
-
-  useEffect(() => {
-    const resetLineup = lineupTimer.resetTimer;
-    const startJam = jamTimer.startTimer;
-    if (lineupTimer.limit) {
-      resetLineup();
-      startJam();
-      setButtonLabel('End Jam');
-    } else if (jamTimer.limit) endJam();
-  }, [
-    lineupTimer.limit,
-    jamTimer.limit,
-    lineupTimer.resetTimer,
-    jamTimer.startTimer,
-    endJam,
-  ]);
-
-  return (
-    <View style={styles.container}>
-      <View style={dimensionBoxStyle}>
-        <Display
-          style={styles.textStyle}
-          limit={periodTimeMS}
-          time={periodTimer.time}
-          countdown={true}
-        />
-      </View>
-      <View style={dimensionBoxStyle}>
-        <Display
-          style={styles.textStyle}
-          limit={jamTimeMS}
-          time={
-            lineupTimer.running
-              ? lineupTimer.time
-              : timeoutTimer.running
-              ? timeoutTimer.time
-              : jamTimer.time
-          }
-          countdown={!lineupTimer.running && !timeoutTimer.running}
-        />
-      </View>
-      <View style={dimensionBoxStyle}>
-        <ControlButton
-          style={styles.textStyle}
-          label={buttonLabel}
-          onPress={
-            lineupTimer.running
-              ? callTimeout
-              : jamTimer.running
-              ? endJam
-              : endTimeout
-          }
-        />
-      </View>
-      <View style={dimensionBoxStyle}>
-        <ControlButton
-          style={styles.textStyle}
-          label={'Reset'}
-          onPress={resetAll}
-        />
-      </View>
-    </View>
-  );
-  */
 };
 
 export default Timer;
--- flat-track-jam-timer/src/hooks/useTimer.js
+++ flat-track-jam-timer/src/hooks/useTimer.js
@@ -1,18 +1,17 @@
-import { useState, useEffect, useRef, useReducer } from 'react';
+import { useReducer } from 'react';
 
 const defaultState = {
   startTime: 0,
   pauseTime: 0,
   maxTime: 0,
   paused: true,
-  limit: false,
   limitTime: 0,
 };
 
 const timerReducer = (state, action) => {
   switch (action.type) {
     case timerDispatch.START:
-      if (!state.paused || state.limit) {
+      if (!state.paused || state.limitTime > 0) {
         return state;
       } else {
         return {
@@ -46,7 +45,6 @@ const timerReducer = (state, action) => {
         paused: true,
         startTime: 0,
         pauseTime: state.maxTime,
-        limit: true,
         limitTime: state.startTime - state.pauseTime + state.maxTime,
       };
     case timerDispatch.RESETANDSTART:
@@ -74,121 +72,6 @@ export const useTimer = (maxTime = 0) => {
     ...defaultState,
     maxTime,
   });
-  /*const [time, setTime] = useState(0);
-
-  const interval = useRef(0);
-
-  useEffect(() => {
-    if (interval.current) {
-      clearInterval(interval.current);
-      interval.current = undefined;
-    }
-    if (timer.startTime > 0 && !timer.paused) {
-      interval.current = setInterval(() => {
-        const newTime = Date.now() - timer.startTime + timer.pauseTime;
-        if (timer.maxTime > 0 && newTime > timer.maxTime) {
-          setTime(timer.maxTime);
-          dispatch({ type: timerDispatch.LIMIT });
-        } else {
-          setTime(newTime);
-        }
-      }, 10);
-    } else {
-      if (timer.pauseTime === 0) setTime(0);
-      if (interval.current) {
-        clearInterval(interval.current);
-        interval.current = undefined;
-      }
-    }
-  }, [
-    timer,
-    /*timer.startTime, timer.paused, timer.maxTime, timer.pauseTime, dispatch,
-  ]);*/
 
   return [timer, dispatch];
 };
-
-/*
-export const useTimer = (timeLimit = 0, limFunc = null) => {
-  const [time, setTime] = useState(0);
-  const [maxTime, setMaxTime] = useState(timeLimit);
-  const [startTime, setStartTime] = useState(0);
-  const [pauseTime, setPauseTime] = useState(0);
-  const [paused, setPaused] = useState(true);
-  const [limit, setLimit] = useState(false);
-  const [limitCall, setLimitCall] = useState(() => limFunc);
-
-  const interval = useRef(0);
-
-  useEffect(() => {
-    console.log(startTime, pauseTime, maxTime, paused, limit);
-    if (startTime > 0 && !paused && !limit) {
-      interval.current = setInterval(() => {
-        const newTime = Date.now() - startTime + pauseTime;
-        if (maxTime > 0 && newTime > maxTime) {
-          setTime(maxTime);
-          setLimit(true);
-          setPaused(true);
-          setStartTime(0);
-        } else {
-          setTime(Date.now() - startTime + pauseTime);
-        }
-      }, 10);
-    } else {
-      if (interval.current) {
-        clearInterval(interval.current);
-        interval.current = undefined;
-      }
-    }
-  }, [startTime, pauseTime, maxTime, paused, limit]);
-
-  useEffect(() => {
-    if (limit && limitCall) {
-      limitCall();
-    }
-  }, [limit, limitCall]);
-
-  const startTimer = useCallback(() => {
-    if (maxTime <= 0 || !limit) {
-      setPaused(false);
-      setStartTime(Date.now());
-    }
-    console.log('Start', time, paused, limit);
-  }, [maxTime, limit, paused]);
-
-  const pauseTimer = useCallback(() => {
-    setPaused(true);
-    setStartTime(0);
-    const currTime = Date.now() - startTime + pauseTime;
-    if (maxTime <= 0 || (!limit && maxTime > currTime)) {
-      setPauseTime(Date.now() - startTime + pauseTime);
-    } else {
-      setTime(maxTime);
-      setLimit(true);
-    }
-  }, [limit, maxTime, pauseTime, startTime]);
-
-  const resetTimer = useCallback((newLimit = 0, newFunc = null) => {
-    setTime(0);
-    if (newLimit > 0) setMaxTime(newLimit);
-    setStartTime(0);
-    setPaused(true);
-    setLimit(false);
-    if (newFunc) {
-      setLimitCall(() => newFunc);
-    }
-    setPauseTime(0);
-    console.log('Reset', time, paused, limit);
-  }, []);
-
-  return {
-    time,
-    running: !paused,
-    limit,
-    startTimer,
-    pauseTimer,
-    setTime,
-    resetTimer,
-    maxTime,
-  };
-};*/
